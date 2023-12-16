bec14292586406c7d6368e754af9f39e79ac65f1
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2023-12-15 20:50:37 -0800

    Double digit combination without repetition

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: Prints two digits combinations without duplication
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int C1;
+	int C2;
+
+	for (C1 = 0; C1 < 10; C1++)
+		for (C2 = C1 + 1; C2 < 10; C2++)
+		{
+			putchar((C1 % 10) + '0');
+			putchar((C2 % 10) + '0');
+
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	putchar('\n');
+	return (0);
+}
