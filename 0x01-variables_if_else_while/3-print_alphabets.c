57a870aa809a0c39ff07a40af2db30b530683de3
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2023-12-15 17:44:18 -0800

    Prints Lower and uppercases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry
+ * Description: Prints upper and lowercase alphabets
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
