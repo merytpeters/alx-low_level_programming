8d3bf2b17f65b854ff7d5dedc9491c8d4213ae4f
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2023-12-15 18:09:28 -0800

    Prints single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Description: Prints Single digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+		printf("%d", x);
+
+	printf("\n");
+
+	return (0);
+}
