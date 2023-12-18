2c6ceb68de7a9bbd7f77a9ed04856901a50a08aa
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2023-12-18 10:14:21 -0800

    Prints two 2 digit combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Description: Prints two two-digit numbers
+ * Description: Prints two 2-digit numbers
  * Return: 0 (Success)
  */
 int main(void)
 {
 	int n1;
 	int n2;
+	int n3;
+	int n4;
 
 	for (n1 = 0; n1 <= 9; n1++)
 		for (n2 = 0; n2 <= 8; n2++)
 		{
-			putchar((n1 % 10) + '0');
-			putchar((n2 % 10) + '0');
-			putchar(' ');
-			putchar((n1 % 10) + '0');
-			putchar((n2 % 10) + '1');
-			if (!(n1 == 9 && n2 == 8))
-
+			for (n3 = 0; n3 <= 9; n3++)
+			for (n4 = 0; n4 <= 8; n4++)
 			{
-				putchar(',');
+				putchar((n1 % 10) + '0');
+				putchar((n2 % 10) + '0');
 				putchar(' ');
+				putchar((n3 % 10) + '0');
+				putchar((n4 % 10) + '1');
+				if (!(n1 == 9 && n2 == 8 && n3 == 9 && n4 == 8))
+
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	putchar('\n');
