89590fdc654f24db57ae563d435c85982d5dfc09
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2023-12-19 14:58:42 -0800

    Prints lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ */
+void print_alphabet(void)
+{
+	char lca;
+
+	lca = 'a'
+		while (lca < 'z')
+			_putchar(lca);
+	_putchar('\n');
+}
