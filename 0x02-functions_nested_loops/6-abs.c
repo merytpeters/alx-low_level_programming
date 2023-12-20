eee0bdcf67865e230303032565435f13276b01cd
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2023-12-19 17:13:33 -0800

    Computes absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of an integer
+ * @i: Any integer
+ * Return: 0 (Success)
+ */
+int _abs(int i)
+{
+	int a;
+	int i;
+
+	a = _abs(int i);
+	_putchar('a');
+}
