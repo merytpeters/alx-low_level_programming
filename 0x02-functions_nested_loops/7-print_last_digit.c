072b020d309cefe0106da8310b9e09f08e7fc904
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2023-12-19 17:37:28 -0800

    Prints last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit
+ * @i: Last digit
+ * Return: 0 (Success)
+ */
+int print_last_digit(int i)
+{
+	for (j = i % 10;)
+		_putchar(j);
+}
