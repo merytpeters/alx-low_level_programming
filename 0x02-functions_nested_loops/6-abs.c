1427a0782bb4d5135ef3beffcb18e0c00ed96142
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2023-12-20 13:37:39 -0800

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,14 +10,8 @@ int _abs(int num)
 	int;
 
 	if (num < 0)
-	{
-		_putchar(num);
-	return (num * -1);
-	}
+		return (num * -1);
 	else
-	{
-		_putchar(num);
-	return (num * 1);
-	}
+		return (num * 1);
 	_putchar('\n');
 }
