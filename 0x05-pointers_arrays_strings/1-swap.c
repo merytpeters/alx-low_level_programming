6b6b4a835496da5ff1db59f3dba7d197c597e4f5
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-02 15:25:47 -0800

    Swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of integers
+ * @a: first integer to swap
+ * @b: second integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
