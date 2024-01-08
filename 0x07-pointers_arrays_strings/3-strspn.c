6e8cbc4fb5667edfc3a7f222e4110d2d5bb002ce
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-08 15:48:00 -0800

    Gets length of a string segment

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *  _strspn - Gets the length of a prefix substring
+ *  @s: Pointer to string to be examined
+ *  @accept: Pointer to string to be matched
+ *  Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int _strspn(char *s, char *accept);
+	unsigned int lth = 0;
+	int l;
+
+	while (*s)
+	{
+		for (l = 0; accept[l]; l++)
+		{
+			if (*s == accept[l])
+			{
+				lth++;
+				break;
+			}
+			else if (accept[l + 1] == '\0')
+			{
+				return (lth);
+			}
+			else
+			{
+				s++;
+			}
+		}
+	}
+	return (lth);
+}
