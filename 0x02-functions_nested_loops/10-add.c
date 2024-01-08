d895607e5744736296e374e04e1fa86f4ade7213
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-08 03:15:10 -0800

    Prints sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Sum of two digits
+ * Description: Adds integers
+ * @i: First integer
+ * @j: Second integer
+ * Return: Sum
+ */
+int add(int i, int j)
+{
+	int sum = i + j;
+
+	return (sum);
+}
