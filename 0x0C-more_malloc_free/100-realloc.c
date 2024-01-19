33644201ed914f0120116739b59d4e03455ea264
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-18 18:57:22 -0800

    Reallocates memory block

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _realloc - Reallocates a memory block using malloc and free
+ * @ptr: Pointer to old memory
+ * @old_size: Number of bytes allocated to ptr
+ * @new_size: Number of bytes of new memory block
+ * Return: Pointer to new memory block or NULL (failure)
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	ptr = malloc(old_size);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
+}
