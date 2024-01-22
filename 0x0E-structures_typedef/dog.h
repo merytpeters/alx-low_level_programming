41f33089926138ebb76a569ef8d3c68b4300b8f2
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-22 11:55:08 -0800

    Prints struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,9 @@ struct dog
 };
 
 /**
- * dogs_info - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-typedef struct dog a_dog;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
