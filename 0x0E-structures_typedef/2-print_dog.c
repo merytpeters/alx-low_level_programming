7929e11b9180a68f0ec5ada86f96d8af1e39f5b5
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-22 11:39:29 -0800

    Prints struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "Name: (nil)";
-	if (d->owner == NULL)
+	if ((*d).owner == NULL)
 		printf("Owner: (nil)\n");
 	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
