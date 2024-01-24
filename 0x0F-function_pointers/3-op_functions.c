d089fa5b21217effa17fdb427751c77ee14b90b7
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-24 15:59:47 -0800

    Operator programme

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds integer
+ * @a: 1st integer
+ * @b: Second int
+ * Return: Sum a , b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Sub integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: Sub a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies numbers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: Result of a, b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides numbers
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: Result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulus
+ * @a: 1st int
+ * @b: 2nd int
+ * Return: Remainder after division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
