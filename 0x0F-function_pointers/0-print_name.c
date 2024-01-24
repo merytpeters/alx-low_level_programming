774432c474eb0b13a7b757b5a48a7261892e7671
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-24 13:51:39 -0800

    Prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - Prints name
+ * @name: Pointer to name
+ * @f: Function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
