5dfc32f91e325c797fbe359cebead540ee124677
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-01 14:44:12 -0800

    Counts elements in list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints elements in a list
+ * @h: Pointer to the head of the linked list
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+
+	if (h == NULL)
+		printf("Empty linked list");
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
