337778a08cd13070c43a768f22b4be977fb33c3b
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-01 15:02:31 -0800

    Prints length of list only

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,18 @@
-#include "main.h"
+#include "lists.h"
 
 /**
- *
+ * list_len - Number of elements only
+ * @h: Pointer to head of list
+ * Return: Number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
