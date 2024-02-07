809f96b3f990ae67b3841d97a537a4b04b7ef235
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-06 16:46:14 -0800

    Function returns number of elements in linked list listint_t

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-int _putchar(char *c);
 /**
  * struct listint_s - singly linked list
  * @n: Integer
@@ -19,6 +18,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 
 #endif
