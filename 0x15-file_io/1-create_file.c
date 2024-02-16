24a6e42abf5afa6203c7e542ea39cfa0e751a5f0
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-16 13:49:38 -0800

    Creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of file
+ * @text_content: Pointer to string
+ * Return: 1 (success), -1 (failure)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+	int wc;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	if (fd)
+	{
+		chmod(filename, 0600);
+		return (1);
+	}
+	else if (fd == -1)
+		return (-1);
+
+	if (filename == NULL)
+		return (-1);
+
+	for (i = 0; text_content[i]; i++)
+	{
+		if (text_content == NULL)
+			return (fd);
+	}
+
+	wc = write(fd, text_content, i);
+
+	if (wc == -1)
+		return (-1);
+	close(fd);
+
+
+	return (1);
+}
