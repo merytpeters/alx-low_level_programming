59ebd2c845620d7aef304a1e82d6f75e56b9cd42
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-16 14:02:55 -0800

    Modified conditional statement to account for file already existing with different permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,6 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd)
 	{
-		if (fd == 0600)
-			return (1);
 		if (fd != 0600)
 			return (1);
 	}
