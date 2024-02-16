2a1782c3ecf551856ba84b3e8277e806b3921a0d
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-16 14:18:07 -0800

    Added a condition for filename

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,11 @@ int create_file(const char *filename, char *text_content)
 	int i;
 	int wc;
 
+	if (!filename)
+		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd)
 	{
 		if (fd != 0600)
