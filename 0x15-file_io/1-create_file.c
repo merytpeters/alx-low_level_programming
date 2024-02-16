e71e76191e6ccd5581610a935ec09d8892de7ccf
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-16 14:48:26 -0800

    Took out some repetitive code lines

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,29 +17,20 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd)
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
 	{
-		if (fd != 0600)
-			return (1);
+		for (i = 0; text_content[i]; i++)
+		{
+			continue;
+		}
+
+		wc = write(fd, text_content, i);
+		if (wc == -1)
+			return (-1);
 	}
-	else if (fd == -1)
-		return (-1);
-
-	if (filename == NULL)
-		return (-1);
-
-	for (i = 0; text_content[i]; i++)
-	{
-		if (text_content == NULL)
-			return (1);
-	}
-
-	wc = write(fd, text_content, i);
-
-	if (wc == -1)
-		return (-1);
 	close(fd);
-
-
 	return (1);
 }
