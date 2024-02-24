9e835b328ef4723723bdfac382340cec2465b382
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-24 08:14:41 -0800

    Output is in reverse, working to fix it without using arrays

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,12 +11,12 @@ void print_binary(unsigned long int n)
 	if (n == 0)
 		_putchar('0');
 
-	while (n > 0)
+	while (n)
 	{
-		if ((n ^ 1) == (n + 1))
-			_putchar('0');
-		else
+		if (n & 1)
 			_putchar('1');
+		else
+			_putchar('0');
 		n >>= 1;
 		i++;
 	}
