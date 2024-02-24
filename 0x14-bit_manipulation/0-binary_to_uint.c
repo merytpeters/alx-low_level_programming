f412c1ee5ab0b30923153d9b8a278483fe965159
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-02-24 04:14:15 -0800

    Restructured and deleted some lines of code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,21 +11,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	unsigned int i;
 
-	for (i = 0; i < strlen(b); i++)
+	if (b == NULL)
+		return (0);
+
+	for (; *b != '\0'; b++)
 	{
-		if (b == NULL)
+		if (*b != '1' && *b != '0')
 			return (0);
-		while (*b != '\0')
-		{
-			if (*b != '1' || *b != '0')
-				return (0);
 
-			num = 1 << i;
-			num += ((*b == '1') ? '1' : '0');
-			b++;
-		}
+		num = (num << 1) + (*b - '0');
 	}
 	return (num);
 }
