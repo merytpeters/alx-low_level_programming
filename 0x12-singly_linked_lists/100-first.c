60fc73cfb011d670d2a1f9708ddc649b73324e97
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-24 13:52:16 +0100

    Creating my own custom function with a message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - Prints out the text given
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
